7c0928d7ca64f999f294b513d7f44b4e22d4de67
miyamoen <miyamoen@users.noreply.github.com>
2015-07-17 00:39:32 +0900

    Update ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,79 +14,20 @@ class ClientController extends Controller
      *
      * @return Response
      */
-    public function index($message)
+    public function getIndex()
     {
-      switch($message){
-      case "log":
-	return view('oleo-client.log');
-      case "phone":
 	return view('oleo-client.index');
-      default:
-	return view('oleo-client.index');
-      }
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
+    
+    public function getLog()
     {
-
+    	return view('oleo-client.log');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return Response
-     */
-    public function store()
+    
+    public function getPhone()
     {
-        //
+    	return view('oleo-client.index')
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    
 }
